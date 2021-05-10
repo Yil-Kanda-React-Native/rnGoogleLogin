e0cd67b0c21c7fc4f132fe6746a49dece71f3d64
YilKanda <yilkanda@hotmail.com>
2021-05-10 13:56:34 -0500

    Firebase connection with the app
    The access was created in Firebase for the app, to be able to access Google, through the plugin @react-native-community/google-signin.
    
    Two buttons were placed, one for logging in and the other for logging out.
    
    When you log in or when you log out, it is displayed in the 'npx react-native start' terminal or in the debug console.
    
    A library was also added to access the Environment Variables and, in that way, hide the sensitive information of the app, such as the web client ID.

--- App.js
+++ App.js
@@ -1,10 +1,65 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View, Button} from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin';
+import {WEB_CLIENT_ID} from '@env';
 
 const App = () => {
   return (
     <View>
-      <Text>Login with Google</Text>
+      <GoogleSigninButton
+        title={'Login with Google'}
+        onPress={() => {
+          GoogleSignin.configure({
+            androidClientId: WEB_CLIENT_ID,
+          });
+          async function signIn() {
+            try {
+              await GoogleSignin.hasPlayServices();
+              const userInfo = await GoogleSignin.signIn();
+              console.log(userInfo);
+            } catch (error) {
+              if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                alert('You cancelled the sign in.');
+              } else if (error.code === statusCodes.IN_PROGRESS) {
+                alert('Google sign In operation is in process');
+              } else if (
+                error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE
+              ) {
+                alert('Play Services not available');
+              } else {
+                alert(
+                  'Something unknown went wrong with Google sign in. ' +
+                    error.message,
+                );
+              }
+            }
+          }
+          signIn();
+        }}
+      />
+
+      <Button
+        title={'Logout with Google'}
+        onPress={() => {
+          GoogleSignin.configure({
+            androidClientId: WEB_CLIENT_ID,
+          });
+          async function signOut() {
+            try {
+              await GoogleSignin.revokeAccess();
+              const userInfo = await GoogleSignin.signOut();
+              console.log(userInfo);
+            } catch (error) {
+              console.error(error);
+            }
+          }
+          signOut();
+        }}
+      />
     </View>
   );
 };
